62f1757743b7c3d658290f3236f0eb5ff8980efb
shohan-pherones <shohan.sub.56@gmail.com>
2022-03-05 20:05:32 +0600

    bug fix done

--- sandbox.js
+++ sandbox.js
@@ -1,17 +1,20 @@
 function fibonacciSeries(seriesNumbers){
-    if(typeof seriesNumbers != 'number'){
-        console.log('Please enter the number.');
+    const fibo = [0, 1];
+    for(let i = 2; i < seriesNumbers; i++){
+    fibo[i] = fibo[i - 1] + fibo[i - 2];
     }
-    else if(seriesNumbers < 2){
-        console.log('Please enter the correct series limit.');
-    }
-    else{
-        const fibo = [0, 1];
-        for(let i = 2; i < seriesNumbers; i++){
-        fibo[i] = fibo[i - 1] + fibo[i - 2];
-        }
-        return fibo;
-    }
+    return fibo;
 }
-const mySeries = fibonacciSeries(15);
-console.log(mySeries);
+
+let seriesNumbers = 20;
+
+if(typeof seriesNumbers != 'number'){
+    console.log('Please enter the number.');
+}
+else if(seriesNumbers < 2){
+    console.log('Please enter the correct series limit.');
+}
+else{
+    const result = fibonacciSeries(seriesNumbers);
+    console.log(result);
+}
